6192bbb321ca8b1b445f51ac8e158af9ed497ed3
yuhcszyt <2502203@163.com>
2017-08-06 11:42:35 +0800

    添加controller

---
+++ src/main/java/com/honganwei/action/LoginAction.java
@@ -0,0 +1,13 @@
+package com.honganwei.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport{
+
+	 public String execute(){
+		
+		 return "login";
+	}
+	
+	
+}
